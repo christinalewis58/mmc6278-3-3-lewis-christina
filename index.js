923a139889e3b45cee41b6dcb9a3fe7a0d357199
Christina Lewis <clewis13@ufl.edu>
2022-05-28 10:08:29 -0400

    created array for questions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+function runQuiz() {
+var questionsArr = [
+    {question: 'The sky is blue.',
+    answer: true
+    },
+    {question: 'The grass is green.',
+    answer: true
+    },
+    {question: 'Birds can swim.',
+    answer: false
+    },
+    {question: 'Dogs are not cute.',
+    answer: false
+    },
+    {question: 'A foot is twelve inches',
+    answer: true
+    }
+    ]
+}
